00682e0307b5c3c9d648cf3ed5f9018471ac3735
bensaadmucret <bensaadmucret@gmail.com>
2025-05-06 12:05:26 +0200

    Ajout de la classe YT_Embed_API pour l'interaction avec l'API YouTube

--- includes/class-yt-embed-api.php
+++ includes/class-yt-embed-api.php
@@ -1,1 +1,29 @@
+<?php
 
+class YT_Embed_API {
+
+    public static function fetch_latest_videos($channel_id, $max = 3) {
+        $api_key = get_option('yt_embed_api_key');
+        if (!$api_key) return [];
+
+        $url = "https://www.googleapis.com/youtube/v3/search?key=$api_key&channelId=$channel_id&part=snippet,id&order=date&maxResults=$max";
+        $response = wp_remote_get($url);
+        if (is_wp_error($response)) return [];
+
+        $data = json_decode(wp_remote_retrieve_body($response), true);
+        if (!isset($data['items'])) return [];
+
+        $videos = [];
+        foreach ($data['items'] as $item) {
+            if ($item['id']['kind'] === 'youtube#video') {
+                $videos[] = [
+                    'video_id' => $item['id']['videoId'],
+                    'title' => $item['snippet']['title'],
+                    'thumbnail' => $item['snippet']['thumbnails']['medium']['url']
+                ];
+            }
+        }
+
+        return $videos;
+    }
+}
