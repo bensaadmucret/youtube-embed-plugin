11e8674e5b47246d24ba5a1304ada0f6bb85658e
bensaadmucret <bensaadmucret@gmail.com>
2025-05-06 12:10:26 +0200

    Ajout de la confirmation de suppression pour les chaînes

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,15 @@
+// admin.js
 
+document.addEventListener("DOMContentLoaded", () => {
+  const deleteButtons = document.querySelectorAll('form button[type="submit"]');
+
+  deleteButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      if (btn.innerText.includes("Supprimer")) {
+        if (!confirm("⚠️ Es-tu sûr de vouloir supprimer cette chaîne ?")) {
+          e.preventDefault();
+        }
+      }
+    });
+  });
+});
